ece5b5048de5bcd1a7af35431d5b843e7703e66f
Patrick Goley <patrick.goley@gmail.com>
2017-11-16 08:33:21 -0600

    using xcodebuild instead of xctool

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -11,6 +11,6 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sh docker_linux_test.sh
 else
-    xctool -project Configurate.xcodeproj -scheme Configurate build test -sdk iphonesimulator GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
+    xcodebuild test -project Configurate.xcodeproj -scheme Configurate-Package ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
     bash <(curl -s https://codecov.io/bash)
 fi
