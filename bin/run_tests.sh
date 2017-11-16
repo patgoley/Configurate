95dc22173cdbf88d3d2ef015a9adf77a4a1c3b35
Patrick Goley <patrick.goley@gmail.com>
2017-11-15 20:42:17 -0600

    attempt at cross platform testing with travis matrix

---
+++ bin/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  run_tests.sh
+#  Configurate
+#
+#  Created by Patrick Goley on 11/15/17.
+#  
+
+set -ev
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sh docker_linux_test.sh
+else
+    xctool -project Configurate.xcodeproj -scheme Configurate build test -sdk iphonesimulator GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
+    bash <(curl -s https://codecov.io/bash)
+fi
