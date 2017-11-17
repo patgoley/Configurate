811f56ea5dd6f33f497cf1ffeb3c0630430ce636
Patrick Goley <patrick.goley@gmail.com>
2017-11-16 21:47:13 -0600

    fixed test script for linux

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #  run_tests.sh
 #  Configurate
@@ -6,11 +6,17 @@
 #  Created by Patrick Goley on 11/15/17.
 #  
 
-set -ev
+OS=""
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sh docker_linux_test.sh
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
+  OS="linux"
+else
+  OS="${2}"
+fi
+
+if [[ "$OS" == "linux" ]]; then
+    docker build . --tag configurate
 else
     xcodebuild test -project Configurate.xcodeproj -scheme Configurate-Package ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
-    bash <(curl -s https://codecov.io/bash)
+    curl -s https://codecov.io/bash | bash
 fi
